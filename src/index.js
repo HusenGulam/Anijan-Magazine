41cf823748fdea006ae1df7a346005b084088e28
Husanboy gulamov <hsencodung77@gmail.com>
2024-06-20 08:50:47 +0500

    new js codes added

--- src/index.js
+++ src/index.js
@@ -57,9 +57,32 @@ window.addEventListener("DOMContentLoaded", () => {
 
     navbarMenuIcon.addEventListener("click",() =>{
         
-        navbarMenuIcon.classList.toggle = "click"
+        let navbarMenu = document.getElementById("header");
+        navbarMenu.classList.toggle("click");
+        
 
     })
+
+
+
+    const obcerver = new IntersectionObserver((entries) =>{
+        entries.forEach((entry) =>{
+            console.log(entry);
+            if(entry.isIntersecting){
+                entry.target.classList.add('show')
+            }
+            else{
+                entry.target.classList.remove('show')
+            } 
+        })
+    })
+    
+    
+    
+    
+    const hiddenElements = document.querySelectorAll('.hidden');
+    
+    hiddenElements.forEach((el) => obcerver.observe(el))
 
 
 
